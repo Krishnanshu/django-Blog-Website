d57cc228d2e68f85d4753200d82bdf9c37dd7717
Krishnanshu Tyagi <krityagi@in.ibm.com>
2021-03-29 20:20:58 +0530

    Uploaded my new website

---
+++ myblog/theblog/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Post, Category
+
+# choices = [('Coding', 'Coding'), ('Gaming', 'Gaming'), ('DevOps', 'DevOps')]
+catchoices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+for item in catchoices:
+    choice_list.append(item)
+
+
+class PostFrom(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+
+        }
+
+
+class EditFrom(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body', 'snippet')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+        }
